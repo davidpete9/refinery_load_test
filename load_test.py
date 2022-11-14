e734d112e53890b07585ef216dea42c1aa3f653f
David Pete <dervid9@gmail.com>
2022-11-14 02:27:45 +0100

    init

---
+++ load_test.py
@@ -0,0 +1,141 @@
+import random
+import uuid
+import _thread
+import time
+import asyncio
+import websockets
+import json
+import logging
+
+from websockets.exceptions import *
+
+from sample_diffs import SampleInput
+
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+
+def get_full_sample(problem_predix):
+    with open('sample.txt', 'r') as f:
+        content = f.read()
+    return dict(fullText=content, resource="{}.problem".format(problem_predix), serviceType='update')
+
+
+class ConnectionHandler:
+
+    def __init__(self, ws_connection, conn_id, fulltext_prob=0.15):
+        self.conn = ws_connection
+        self.conn_id = conn_id
+
+        self.problem_prefix = uuid.uuid1().hex[:8]
+        self.state_id = "-80000000"
+        self.fulltext_prob = fulltext_prob
+        self.log = dict()
+
+        self.cnt = 0
+        self.message_cnt = 0
+
+    def get_connection(self):
+        return self.conn
+
+    def get_next_message(self, is_first_message=False):
+        new_id = uuid.uuid1().hex
+        if random.random() < self.fulltext_prob or is_first_message:
+            self.message_cnt = 0
+            return dict(id=new_id, request=get_full_sample(self.problem_prefix))
+        inp = SampleInput.INPUT_ARR[self.message_cnt]
+        self.message_cnt = 0 if self.message_cnt + 1 >= len(SampleInput.INPUT_ARR) else self.message_cnt + 1
+        to_send = dict(**inp, resource=f'{self.problem_prefix}.problem')
+        if inp['serviceType'] == 'occurrences':
+            to_send['expectedStateId'] = self.state_id
+        else:
+            to_send['requiredStateId'] = self.state_id
+
+        return dict(id=new_id, request=to_send)
+
+    async def send_message(self):
+        next_message = self.get_next_message(False if self.cnt != 0 else True)
+
+        self.log[next_message['id']] = dict(cnt=self.cnt)
+        self.cnt += 1
+        try:
+            await self.conn.send(json.dumps(next_message))
+            print('Send')
+            print(next_message)
+        except ConnectionClosedOK as e:
+            print('Connection closed ok {}'.format(e.reason))
+            self.log[next_message['id']]['state'] = 'Rejected'
+        except ConnectionClosedError as e:
+            print('Connection closed error {}'.format(e.reason))
+            self.log[next_message['id']]['state'] = 'ClientErr'
+        finally:
+            self.log[next_message['id']]['sent_at'] = time.time()
+
+    async def consumer_handler(self):
+        async for message in self.conn:
+            self.process_message(message)
+
+    def process_message(self, message):
+        arrived_at = time.time()
+        try:
+            obj = json.loads(message)
+            print(obj)
+            if 'id' in obj and obj['id'] in self.log:
+                self.state_id = obj['response']['stateId'] if 'response' in obj and 'stateId' in obj[
+                    'response'] else self.state_id
+                if self.cnt != self.log[obj['id']]['cnt'] + 1:
+                    self.log[obj['id']]['state'] = 'late'
+                else:
+                    self.log[obj['id']]['state'] = 'OK'
+                self.log[obj['id']]['response_at'] = arrived_at
+                self.log[obj['id']]['resp_cnt'] = self.log[obj['id']]['resp_cnt'] + 1 \
+                    if 'resp_cnt' in self.log[obj['id']] else 1
+                self.log[obj['id']]['resp_size'] = self.log[obj['id']]['resp_size'] + len(message) \
+                    if 'resp_size' in self.log[obj['id']] else len(message)
+        except Exception as e:
+            pass
+
+
+class StressTester:
+
+    async def init_connections(self):
+        for i in range(0, self.conn_count):
+            conn = await websockets.connect(self.url, logger=logger,
+                                            subprotocols=['tools.refinery.language.web.xtext.v1'])
+            conn_handler = ConnectionHandler(conn, i)
+            self.connections.append(conn_handler)
+            # Start async process to receive all messages of this connection.
+            asyncio.create_task(conn_handler.consumer_handler())
+
+    def __init__(self, ws_url, load_profile, num_of_connections, full_text_prob=0.1, max_diff_words=10):
+        self.url = ws_url
+        self.conn_count = num_of_connections
+        self.connections = []
+
+    def send_message_everywhere(self):
+        for c in self.connections:
+            asyncio.create_task(c.send_message())
+
+
+WS_URL = 'wss://xtext.test.refinery.services/xtext-service'
+
+
+async def start_testing():
+    t = StressTester(WS_URL, [], 60)
+    await t.init_connections()
+
+    for i in range(1000):
+        t.send_message_everywhere()
+        await asyncio.sleep(0.3)
+    await asyncio.sleep(5)
+    for c in t.connections:
+        await c.get_connection().close()
+        with open(f'stats/{c.conn_id}','w+') as f:
+            f.write([','.join([c.log[key]['cnt'],
+                      c.log[key]['sent_at']-(c.log[key]['response_at'] if 'response_at' in c.log[key]  else c.log[key]['sent_at']),
+                      c.log[key]['state']]) for key in c.log])
+
+
+
+asyncio.run(start_testing())
---
+++ sample_diffs.py
@@ -0,0 +1,14 @@
+
+
+class SampleInput:
+    INPUT_ARR = [
+        dict(deltaOffset=236, deltaReplaceLength=0,deltaText="class Pet {\n  Person owner\n  int age\n  string name\n}",serviceType="update"),
+        dict(caretOffset=352, deltaOffset=349, deltaReplaceLength=6,deltaText="Pet", proposalsLimit=1000, serviceType="assist"),
+        dict(caretOffset=328, serviceType="occurrences"),
+        dict(deltaOffset=43,deltaReplaceLength=0, deltaText="Pet dog", serviceType="update"),
+        dict(deltaOffset=247,deltaReplaceLength=52,deltaText="", serviceType="update"),
+        dict(caretOffset=50, deltaOffset=48, deltaReplaceLength=3, deltaText="Pe", proposalsLimit=1000, serviceType="assist"),
+        dict(deltaOffset=50,deltaReplaceLength=0,deltaText="rson",serviceType="update"),
+        dict(deltaOffset=315,deltaReplaceLength=3,deltaText="Person",serviceType="update"),
+        dict(caretOffset=130, serviceType="occurrences")
+    ]
