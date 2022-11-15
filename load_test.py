3b62df11a6d43ccd62c6f14ea993988885bb541a
David Pete <dervid9@gmail.com>
2022-11-16 00:59:21 +0100

    add metrics

--- load_test.py
+++ load_test.py
@@ -11,9 +11,83 @@ from websockets.exceptions import *
 
 from sample_diffs import SampleInput
 
-logger = logging.getLogger('websockets')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
+# logger = logging.getLogger('websockets')
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(logging.StreamHandler())
+
+from opentelemetry import metrics
+from opentelemetry.exporter.prometheus_remote_write import (
+    PrometheusRemoteWriteMetricsExporter,
+)
+from opentelemetry.metrics import Observation
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+
+exporter = PrometheusRemoteWriteMetricsExporter(
+    endpoint="http://localhost:8080/workspaces/ws-d59302a8-3858-4815-83e3-4e6519150ea4/api/v1/remote_write",
+    headers={
+        "host": "aps-workspaces.us-east-1.amazonaws.com"
+    }
+)
+reader = PeriodicExportingMetricReader(exporter, 1000)
+provider = MeterProvider(metric_readers=[reader])
+metrics.set_meter_provider(provider)
+meter = metrics.get_meter(__name__)
+
+
+OK_Message_Num = 1
+Late_Message_NUM = 0
+
+def get_late_raito(observer):
+    global Late_Message_NUM
+    global OK_Message_Num
+    raito = Late_Message_NUM / OK_Message_Num
+    Late_Message_NUM = 0
+    OK_Message_Num = 0
+    yield Observation(raito, {})
+
+msg_sent_counter = meter.create_counter(
+    name="messages sent",
+    description="number of messages",
+    unit="1",
+)
+
+msg_received_counter = meter.create_counter(
+    name="ws messages received",
+    description="number of messages",
+    unit="1",
+)
+
+ws_conn_counter = meter.create_up_down_counter(
+    name="ws connections open",
+    description="number of messages",
+    unit="1",
+)
+
+ws_errors = meter.create_counter(
+    name="ws errors",
+    description="Errors.",
+    unit="1"
+)
+
+late_raito = meter.create_observable_gauge(
+    callbacks=[get_late_raito],
+    name="late message raito",
+    description="",
+    unit="1"
+)
+
+resp_time_hg = meter.create_histogram(
+    name="Response time",
+    description="Response time of messages",
+    unit="1"
+)
+
+resp_size_hg = meter.create_histogram(
+    name="Response size",
+    description="Response size of messages",
+    unit="1"
+)
 
 
 def get_full_sample(problem_predix):
@@ -69,13 +143,13 @@ class ConnectionHandler:
         self.cnt += 1
         try:
             await self.conn.send(json.dumps(next_message))
-            print('Send')
-            print(next_message)
+            msg_sent_counter.add(1)
         except ConnectionClosedOK as e:
             print('Connection closed ok {}'.format(e.reason))
             self.close_reason = 'server_closed'
             self.log[next_message['id']]['state'] = 'server_closed'
             self.is_closed = True
+            ws_errors.add(1)
         except ConnectionClosedError as e:
             print('Connection closed error {}'.format(e.reason))
             self.is_closed = True
@@ -92,19 +166,25 @@ class ConnectionHandler:
         arrived_at = time.time()
         try:
             obj = json.loads(message)
-            print(obj)
+            msg_received_counter.add(1)
             if 'id' in obj and obj['id'] in self.log:
                 self.state_id = obj['response']['stateId'] if 'response' in obj and 'stateId' in obj[
                     'response'] else self.state_id
                 if self.cnt != self.log[obj['id']]['cnt'] + 1:
+                    global Late_Message_NUM
+                    Late_Message_NUM += 1
                     self.log[obj['id']]['state'] = 'late'
                 else:
+                    global OK_Message_Num
+                    OK_Message_Num += 1
                     self.log[obj['id']]['state'] = 'OK'
                 self.log[obj['id']]['response_at'] = arrived_at
+                resp_time_hg.record(arrived_at - self.log[obj['id']]['sent_at'])
                 self.log[obj['id']]['resp_cnt'] = self.log[obj['id']]['resp_cnt'] + 1 \
                     if 'resp_cnt' in self.log[obj['id']] else 1
                 self.log[obj['id']]['resp_size'] = self.log[obj['id']]['resp_size'] + len(message) \
                     if 'resp_size' in self.log[obj['id']] else len(message)
+                resp_size_hg.record(self.log[obj['id']]['resp_size'])
         except Exception as e:
             pass
 
@@ -115,7 +195,7 @@ class StressTester:
         for i in range(0, conn_num):
             conn = await websockets.connect(self.url,
                                             subprotocols=['tools.refinery.language.web.xtext.v1'])
-            ind = len(self.connections)+1
+            ind = len(self.connections) + 1
             conn_handler = ConnectionHandler(conn, ind)
             self.connections.append(conn_handler)
             # Start async process to receive all messages of this connection.
@@ -123,12 +203,13 @@ class StressTester:
 
     async def update_connection_num(self, conn_num):
         if conn_num < self.conn_count:
-            to_close = self.conn_count-conn_num
+            to_close = self.conn_count - conn_num
             for i in range(0, to_close):
                 await self.connections[i].close_connection()
         if self.conn_count < conn_num:
-            await self.init_connections(conn_num-self.conn_count)
+            await self.init_connections(conn_num - self.conn_count)
         self.conn_count = conn_num
+        ws_conn_counter.add(conn_num)
 
     def __init__(self, ws_url, num_of_connections):
         self.url = ws_url
@@ -142,8 +223,7 @@ class StressTester:
 
 WS_URL = 'wss://xtext.test.refinery.services/xtext-service'
 
-
-load_profile = [dict(time_wait=1, itherations=1, conns=1),dict(time_wait=0.5, itherations=1, conns=2)]
+load_profile = [dict(time_wait=1, itherations=1, conns=1), dict(time_wait=0.5, itherations=1, conns=2)]
 
 
 def get_stats(handlers):
@@ -163,7 +243,9 @@ def get_stats(handlers):
 async def start_testing():
     t = StressTester(WS_URL, 0)
     for l in load_profile:
-        print('Running load profile: time_between = {}  itherations = {} conns =  {}'.format(l['time_wait'],l['itherations'], l['conns']))
+        print('Running load profile: time_between = {}  itherations = {} conns =  {}'.format(l['time_wait'],
+                                                                                             l['itherations'],
+                                                                                             l['conns']))
         await t.update_connection_num(l['conns'])
         for i in range(0, l['itherations']):
             t.send_message_everywhere()
@@ -174,7 +256,7 @@ async def start_testing():
     get_stats(t.connections)
     for c in t.connections:
         await c.get_connection().close()
-        #with open(f'stats/{c.conn_id}.csv','w+') as f:
+        # with open(f'stats/{c.conn_id}.csv','w+') as f:
         #    f.write('\n'.join([';'.join(
         #              [
         #              str(c.log[key]['cnt']),
@@ -185,7 +267,6 @@ async def start_testing():
         #              str(c.log[key]['resp_size'])
         #              ])
         #              for key in c.log]))
-
 
 
 asyncio.run(start_testing())
